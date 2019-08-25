1c13be53a5720ce9b1b992cc046b72aab8bc625a
Hideo Kojima <kostyaelizarov@mail.ru>
2019-08-26 01:16:37 +0300

    BadPenek Started

---
+++ UnityProject/Assets/SCRIPTS!!!!!!!!!!!!!!!!!!!!/dialogueSystem/DialogueMass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueMass
+{
+    public static string[] Rus =
+    {
+            "LexusLight", //0
+            "При поддержке \n Abysmalduck", //1
+            "GoldenSoul.",//2
+            "~ Надежда. Вера. Судьба. \n Всё тщетно...",//3
+            "~ Каждый, кто попадает в зачарованный лес, остаётся здесь навсегда.",//4
+            "~ Это просто плохой сон?",//5
+            "~ Нет, ведь ты не спал буд-то целую вечность.",//6
+            "Всё что осталось, это твоё имя. \nПотеря имени равносильна потере всего, что у вас было.",//7
+            "Тебя зовут Каспиан.",//8
+            "Пожалуйста, запомни это имя.",//9
+ /*cat1*/   
+    };
+    public static string[] Eng =
+{
+            "LexusLight", //0
+            "together with \n Abysmalduck",//1
+            "GoldenSoul.",//2
+            "~ Hope. Faith. Fate. \nAll in vain.",//3
+            "~ All who's included in the Enchanted Forest,stays there forever.",//4
+            "~ Maybee is this just a bad dream?",//5
+            "~ No, because you haven't slept eternity.",//6
+            "All that's left is your name. \nLosing a name is tantamount to losing everything.",//7
+            "Your name is Caspian.",//8
+            "Please remember it.",//9
+            "1",//10
+            "2",//11
+            "3",//12
+            "4",//13
+            "5",//14
+            "6",//15
+            "7",//16
+            "8",//17
+    };
+}
---
+++ UnityProject/Assets/SCRIPTS!!!!!!!!!!!!!!!!!!!!/ПЕРСОНАЖ/Character1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+
+public class Character1 : MonoBehaviour
+{
+    public GameObject Life;
+    static GameObject _Life;
+
+    void Update()
+    {
+        try
+        {
+            if (_Life.GetComponent<Animator>().GetInteger("Stage") < 1)
+            {
+                Debug.Log(1);
+            }
+        }
+        catch {}
+
+    }
+}
