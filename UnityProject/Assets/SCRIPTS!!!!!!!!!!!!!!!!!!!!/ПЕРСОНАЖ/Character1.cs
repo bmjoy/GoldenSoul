e4e99d3ac6aac95e563eaaef9d3eed8d05191b8c
Hideo Kojima <kostyaelizarov@mail.ru>
2019-08-26 22:36:17 +0300

    Controller + penek + move

--- UnityProject/Assets/SCRIPTS!!!!!!!!!!!!!!!!!!!!/ПЕРСОНАЖ/Character1.cs
+++ UnityProject/Assets/SCRIPTS!!!!!!!!!!!!!!!!!!!!/ПЕРСОНАЖ/Character1.cs
@@ -9,17 +9,43 @@ public class Character1 : MonoBehaviour
 {
     public GameObject Life;
     static GameObject _Life;
+    public GameObject Lifepoint;
+    static GameObject _Lifepoint;
+    private GameObject Player;
 
+    static int AttackVector = 1;
+    static int AlertPoints = 0;//Нужно для появления индикатора
+
+    private void Start()
+    {
+        _Life = Life;
+        _Lifepoint = Lifepoint;
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
     void Update()
     {
-        try
-        {
-            if (_Life.GetComponent<Animator>().GetInteger("Stage") < 1)
-            {
-                Debug.Log(1);
-            }
-        }
-        catch {}
 
+    if (_Life.GetComponent<Animator>().GetInteger("Stage") < 2)
+    {
+        Debug.Log("Death");
     }
+        AttackVector =(Player.GetComponent<Animator>().GetInteger("vector") != 6) ? Player.GetComponent<Animator>().GetInteger("vector") : AttackVector;
+    }
+
+    static public void MinusHp()
+    {
+        _Life.GetComponent<Animator>().SetInteger("Stage", (_Life.GetComponent<Animator>().GetInteger("Stage") - 1));
+    }
+
+    static public void Alert()
+    {
+        AlertPoints++;
+        _Lifepoint.SetActive(true);
+    }
+    static public void NoAlert()
+    {
+        AlertPoints--;
+        if (AlertPoints < 1) _Lifepoint.SetActive(false);
+    }
+
 }
