e20e70c48709abc66d9883745ed12011a0b98452
Abysmalduck <unitu696@gmail.com>
2019-09-25 20:55:56 +0300

    mobileFPS fix

---
+++ UnityProject/Assets/SCRIPTS!!!!!!!!!!!!!!!!!!!!/Tech scripts/test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private void OnGUI()
+    {
+        //GUI.Label(new Rect(100,100,100,100),Convert.ToString(Screen.width));
+    }
+}
