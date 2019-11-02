f12dacea59ee177ed051bf407932fdf89be99ba6
Kirill Kudryavtsev <deissh@yandex.ru>
2019-11-02 17:57:59 +0300

    feat(DiscordRPC): Rich Presence Visualizer

---
+++ UnityProject/Assets/SCRIPTS/DiscordController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class DiscordJoinEvent : UnityEngine.Events.UnityEvent<string> { }
+
+[System.Serializable]
+public class DiscordSpectateEvent : UnityEngine.Events.UnityEvent<string> { }
+
+[System.Serializable]
+public class DiscordJoinRequestEvent : UnityEngine.Events.UnityEvent<DiscordRpc.JoinRequest> { }
+
+public class DiscordController : MonoBehaviour
+{
+    private int callbackCalls;
+    public static string applicationId = "640171798184591360";
+    public static string optionalSteamId;
+
+    public DiscordRpc.RichPresence presence = new DiscordRpc.RichPresence();
+    public DiscordRpc.JoinRequest joinRequest;
+    public UnityEngine.Events.UnityEvent onConnect;
+    public UnityEngine.Events.UnityEvent onDisconnect;
+    public DiscordJoinEvent onJoin;
+    public DiscordJoinEvent onSpectate;
+    public DiscordJoinRequestEvent onJoinRequest;
+
+    DiscordRpc.EventHandlers handlers;
+
+    public void ReadyCallback()
+    {
+        ++callbackCalls;
+        Debug.Log("Discord: ready");
+        onConnect.Invoke();
+
+        presence.state = "Главное меню";
+        presence.largeImageText = "GoldenSoul";
+        presence.largeImageKey = "3vawmmep0is";
+        DiscordRpc.UpdatePresence(presence);
+    }
+
+    public void DisconnectedCallback(int errorCode, string message)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: disconnect {0}: {1}", errorCode, message));
+        onDisconnect.Invoke();
+    }
+
+    public void ErrorCallback(int errorCode, string message)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: error {0}: {1}", errorCode, message));
+    }
+
+    public void JoinCallback(string secret)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: join ({0})", secret));
+        onJoin.Invoke(secret);
+    }
+
+    public void SpectateCallback(string secret)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: spectate ({0})", secret));
+        onSpectate.Invoke(secret);
+    }
+
+    public void RequestCallback(ref DiscordRpc.JoinRequest request)
+    {
+        ++callbackCalls;
+        Debug.Log(string.Format("Discord: join request {0}#{1}: {2}", request.username, request.discriminator, request.userId));
+        joinRequest = request;
+        onJoinRequest.Invoke(request);
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        DiscordRpc.RunCallbacks();
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("Discord: init");
+        callbackCalls = 0;
+
+        handlers = new DiscordRpc.EventHandlers();
+        handlers.readyCallback = ReadyCallback;
+        handlers.disconnectedCallback += DisconnectedCallback;
+        handlers.errorCallback += ErrorCallback;
+        handlers.joinCallback += JoinCallback;
+        handlers.spectateCallback += SpectateCallback;
+        handlers.requestCallback += RequestCallback;
+        DiscordRpc.Initialize(applicationId, ref handlers, true, optionalSteamId);
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Discord: shutdown");
+        DiscordRpc.Shutdown();
+    }
+
+    void OnDestroy()
+    {
+
+    }
+}
