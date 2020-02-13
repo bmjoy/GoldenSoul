95f31d6e738be0f1643616e3acb297661c755e15
Hideo Kojima <kostyaelizarov@mail.ru>
2020-02-13 23:03:24 +0300

    a

---
+++ UnityProject/Assets/4 SCRIPTS/Tech scripts/ActiveOnStep.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveOnStep : MonoBehaviour
+{
+    public GameObject Obj;
+    private void Start()
+    {
+        Obj.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            Obj.SetActive(true);
+    }
+}
---
+++ UnityProject/Assets/4 SCRIPTS/Tech scripts/DeactiveOnKill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactiveOnKill : MonoBehaviour
+{
+    public State[] States;
+    public GameObject ObjectDeactive;
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (State S in States)
+        {
+            if (S.dead == false) return;
+        }
+        ObjectDeactive.SetActive(false);
+    }
+}
